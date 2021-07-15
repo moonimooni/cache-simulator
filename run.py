d3513d142360186405c36e40d7b991b6ce7e82f0
moonimooni <munhw1408@gmail.com>
2021-07-15 17:33:31 +0900

    작동 확인

--- run.py
+++ run.py
@@ -2,17 +2,33 @@ from simulator import CacheSimulator
 
 
 def simluate_cache():
-    cache_size = int(input("최대 캐시 사이즈를 입력하세요 : "))
-    hit_time = int(input("캐시 히트시 소요시간을 입력하세요 : "))
-    miss_time = int(input("캐시 미스시 소요시간을 입력하세요 : "))
-    stocks = list(input("주식 종목의 시퀀스를 입력하세요 : "))
+    cache_size = int(input("Please enter maximum cache size : "))
+    hit_time = int(input("Please enter cache hit time : "))
+    miss_time = int(input("Please enter cache miss time : "))
+    stocks = list(
+        map(str, input("Please enter sequence of stocks : ").split()))
 
     cache = CacheSimulator(hit_time, miss_time)
+    simulation_result = list()
 
     for size in range(1, cache_size):
-        simulation_result = list()
         simulation_result.append(cache.simulate(stocks, size))
-        return simulation_result
+
+    return simulation_result
 
 
-print(simluate_cache())
+def find_best_cache():
+    simluation_results = simluate_cache()
+
+    min_runtime = min(simluation_results,
+                      key=lambda x: x["runtime"])["runtime"]
+
+    simluation_results = [
+        result for result in simluation_results if result["runtime"] == min_runtime]
+
+    simluation_results.sort(key=lambda x: x["cache size"])
+    smallest_cache_size = simluation_results[0]["cache size"]
+    return smallest_cache_size
+
+
+print("best cache size : ", find_best_cache())
--- simulator.py
+++ simulator.py
@@ -8,35 +8,38 @@ class CacheSimulator:
         self.hit_time = hit_time
         self.miss_time = miss_time
 
-    def find_cache(self, data, cache_map, cache_used_order):
-        if cache_map[data]:
+    def find_cache(self, data, cache_map, cache_used_order, cache_size):
+        if data in cache_map:
             self.hit_count += 1
             self.runtime += self.hit_time
-            recent_data = cache_used_order.remove(data)
+            cache_used_order.remove(data)
 
         else:
             self.miss_count += 1
             self.runtime += self.miss_time
 
-            if len(cache_map) == self.cache_size:
+            if len(cache_map) == cache_size:
                 deleted_data = cache_used_order.pop()
                 del cache_map[deleted_data]
-                recent_data = data
-
+            
+        recent_data = data
         cache_used_order.appendleft(recent_data)
         cache_map[recent_data] = recent_data
         return
 
     def simulate(self, stocks, size):
         cache_map = dict()
-        cache_used_order = deque([None for _ in range(size)])
+        cache_used_order = deque([])
 
         for stock in stocks:
-            self.find_cache(stock, cache_map, cache_used_order)
+            self.find_cache(stock, cache_map, cache_used_order, size)
 
-        return {
+        result = {
             "cache size": size,
             "hit count": self.hit_count,
             "miss count": self.miss_count,
             "runtime": self.runtime
         }
+        self.hit_count, self.miss_count, self.runtime = 0, 0, 0
+        
+        return result
